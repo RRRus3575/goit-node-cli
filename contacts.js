382d0e60a5562fca0f921d0ac62efee73ebc269e
Ruslan <ruslan.grudistov@gmail.com>
2025-03-19 17:20:38 +0200

    path

--- contacts.js
+++ contacts.js
@@ -1,7 +1,15 @@
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
+import fs from "node:fs/promises"
+import  path  from "node:path"
+import {fileURLToPath} from "node:url"
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+console.log(contactsPath)
+
 
 async function listContacts() {
     // ...твій код. Повертає масив контактів.
